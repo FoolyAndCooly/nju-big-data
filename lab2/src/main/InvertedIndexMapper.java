1e9822d15152f6c712b294af031b1c66f976d04c
liuyicheng <1764236936@qq.com>
2024-05-06 15:22:20 +0800

    split by blank and ,

--- lab2/src/main/InvertedIndexMapper.java
+++ lab2/src/main/InvertedIndexMapper.java
@@ -1,7 +1,6 @@
 package src.main;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -17,9 +16,9 @@ public class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text> 
         String fileName = fileSplit.getPath().getName();
         Text word = new Text();
         Text fileName_lineOffset = new Text(fileName);
-        StringTokenizer itr = new StringTokenizer(value.toString());
-        for (;itr.hasMoreTokens();) {
-            word.set(itr.nextToken());
+        String[] buffer = value.toString().split("[\\s,]+");
+        for (String itr : buffer) {
+            word.set(itr);
             context.write(word, fileName_lineOffset);
         }
     }
